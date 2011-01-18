6c2bd5ea4c27e141450bf5297b89df2e013df541
Max Tsepkov <max@garygolden.me>
2011-01-18 13:23:16 +0500

    color_dump: format and add functions and variables list

--- bash_color.sh
+++ bash_color.sh
@@ -36,20 +36,48 @@ COLOR_WHITEB=47           # set white background
 
 
 # Outputs colors table
-function colors_dump {
-    T='gYw'   # The test text
-echo -e "\n                 40m     41m     42m     43m\
-     44m     45m     46m     47m";
+function color_dump
+{
+    local text='gYw'
 
-for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
-           '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
-           '  36m' '1;36m' '  37m' '1;37m';
-  do FG=${FGs// /}
-  echo -en " $FGs \033[$FG  $T  "
-  for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
-    do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
-  done
-  echo;
-done
-echo
+    echo -e "\n                 40m     41m     42m     43m     44m     45m     46m     47m";
+
+    for FGs in '   0m' '   1m' '  30m' '1;30m' '  31m' '1;31m' \
+               '  32m' '1;32m' '  33m' '1;33m' '  34m' '1;34m' \
+               '  35m' '1;35m' '  36m' '1;36m' '  37m' '1;37m';
+    do
+        FG=${FGs// /}
+        echo -en " $FGs \033[$FG  $T  "
+        for BG in 40m 41m 42m 43m 44m 45m 46m 47m; do
+            echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
+        done
+        echo;
+    done
+
+    #color_bold "    Code   Function           Variable"
+    echo '
+    0        color_reset          $COLOR_RESET
+    1        color_bold           $COLOR_BOLD
+    2        color_bright         $COLOR_BRIGHT
+    4        color_underscore     $COLOR_UNDERSCORE
+    7        color_reverse        $COLOR_REVERSE
+
+    30       color_black          $COLOR_BLACK
+    31       color_red            $COLOR_RED
+    32       color_green          $COLOR_GREEN
+    33       color_brown          $COLOR_BROWN
+    34       color_blue           $COLOR_BLUE
+    35       color_magenta        $COLOR_MAGENTA
+    36       color_cyan           $COLOR_CYAN
+    37       color_white          $COLOR_WHITE
+
+    40       color_blackb         $COLOR_BLACKB
+    41       color_redb           $COLOR_REDB
+    42       color_greenb         $COLOR_GREENB
+    43       color_brownb         $COLOR_BROWNB
+    44       color_blueb          $COLOR_BLUEB
+    45       color_magentab       $COLOR_MAGENTAB
+    46       colob_cyanb          $COLOR_CYANB
+    47       color_whiteb         $COLOR_WHITEB
+'
 }
