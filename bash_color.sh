46626ba3cd256cdf074e06e2c486cc840920fe14
Max Mercuriev <max@mercuriev.dev>
2011-01-18 13:25:13 +0500

    color_dump: remove strange $EINS variable

--- bash_color.sh
+++ bash_color.sh
@@ -49,7 +49,7 @@ function color_dump
         FG=${FGs// /}
         echo -en " $FGs \033[$FG  $T  "
         for BG in 40m 41m 42m 43m 44m 45m 46m 47m; do
-            echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
+            echo -en " \033[$FG\033[$BG  $T  \033[0m";
         done
         echo;
     done
