75caf7a1e1a91463b0432172e066f9036b799341
Max Mercuriev <max@mercuriev.dev>
2011-01-19 19:28:09 +0500

    replace all occurences of color with clr

--- bash_color.sh
+++ bash_color.sh
@@ -2,48 +2,48 @@
 #
 # Library for terminal text coloring.
 
-COLOR_ESC="\033["
+CLR_ESC="\033["
 
 # All these varibles has a function with the same name, but in lower case.
 #
-COLOR_RESET=0             # reset all attributes to their defaults
-COLOR_RESET_UNDERLINE=24  # underline off
-COLOR_RESET_REVERSE=27    # reverse off
-COLOR_DEFAULT=39          # set underscore off, set default foreground color
-COLOR_DEFAULTB=49         # set default background color
+CLR_RESET=0             # reset all attributes to their defaults
+CLR_RESET_UNDERLINE=24  # underline off
+CLR_RESET_REVERSE=27    # reverse off
+CLR_DEFAULT=39          # set underscore off, set default foreground color
+CLR_DEFAULTB=49         # set default background color
 
-COLOR_BOLD=1              # set bold
-COLOR_BRIGHT=2            # set half-bright (simulated with color on a color display)
-COLOR_UNDERSCORE=4        # set underscore (simulated with color on a color display)
-COLOR_REVERSE=7           # set reverse video
+CLR_BOLD=1              # set bold
+CLR_BRIGHT=2            # set half-bright (simulated with color on a color display)
+CLR_UNDERSCORE=4        # set underscore (simulated with color on a color display)
+CLR_REVERSE=7           # set reverse video
 
-COLOR_BLACK=30            # set black foreground
-COLOR_RED=31              # set red foreground
-COLOR_GREEN=32            # set green foreground
-COLOR_BROWN=33            # set brown foreground
-COLOR_BLUE=34             # set blue foreground
-COLOR_MAGENTA=35          # set magenta foreground
-COLOR_CYAN=36             # set cyan foreground
-COLOR_WHITE=37            # set white foreground
+CLR_BLACK=30            # set black foreground
+CLR_RED=31              # set red foreground
+CLR_GREEN=32            # set green foreground
+CLR_BROWN=33            # set brown foreground
+CLR_BLUE=34             # set blue foreground
+CLR_MAGENTA=35          # set magenta foreground
+CLR_CYAN=36             # set cyan foreground
+CLR_WHITE=37            # set white foreground
 
-COLOR_BLACKB=40           # set black background
-COLOR_REDB=41             # set red background
-COLOR_GREENB=42           # set green background
-COLOR_BROWNB=43           # set brown background
-COLOR_BLUEB=44            # set blue background
-COLOR_MAGENTAB=45         # set magenta background
-COLOR_CYANB=46            # set cyan background
-COLOR_WHITEB=47           # set white background
+CLR_BLACKB=40           # set black background
+CLR_REDB=41             # set red background
+CLR_GREENB=42           # set green background
+CLR_BROWNB=43           # set brown background
+CLR_BLUEB=44            # set blue background
+CLR_MAGENTAB=45         # set magenta background
+CLR_CYANB=46            # set cyan background
+CLR_WHITEB=47           # set white background
 
 # General function to wrap string with escape seqence(s).
-# Ex: console_escape foobar $COLOR_RED $COLOR_BOLD
+# Ex: console_escape foobar $CLR_RED $CLR_BOLD
 function clr_escape
 {
     echo $1
 }
 
 # Outputs colors table
-function color_dump
+function clr_dump
 {
     local text='gYw'
 
@@ -61,31 +61,31 @@ function color_dump
         echo;
     done
 
-    #color_bold "    Code   Function           Variable"
+    #clr_bold "    Code   Function           Variable"
     echo '
-    0        color_reset          $COLOR_RESET
-    1        color_bold           $COLOR_BOLD
-    2        color_bright         $COLOR_BRIGHT
-    4        color_underscore     $COLOR_UNDERSCORE
-    7        color_reverse        $COLOR_REVERSE
+    0        clr_reset          $CLR_RESET
+    1        clr_bold           $CLR_BOLD
+    2        clr_bright         $CLR_BRIGHT
+    4        clr_underscore     $CLR_UNDERSCORE
+    7        clr_reverse        $CLR_REVERSE
 
-    30       color_black          $COLOR_BLACK
-    31       color_red            $COLOR_RED
-    32       color_green          $COLOR_GREEN
-    33       color_brown          $COLOR_BROWN
-    34       color_blue           $COLOR_BLUE
-    35       color_magenta        $COLOR_MAGENTA
-    36       color_cyan           $COLOR_CYAN
-    37       color_white          $COLOR_WHITE
+    30       clr_black          $CLR_BLACK
+    31       clr_red            $CLR_RED
+    32       clr_green          $CLR_GREEN
+    33       clr_brown          $CLR_BROWN
+    34       clr_blue           $CLR_BLUE
+    35       clr_magenta        $CLR_MAGENTA
+    36       clr_cyan           $CLR_CYAN
+    37       clr_white          $CLR_WHITE
 
-    40       color_blackb         $COLOR_BLACKB
-    41       color_redb           $COLOR_REDB
-    42       color_greenb         $COLOR_GREENB
-    43       color_brownb         $COLOR_BROWNB
-    44       color_blueb          $COLOR_BLUEB
-    45       color_magentab       $COLOR_MAGENTAB
-    46       colob_cyanb          $COLOR_CYANB
-    47       color_whiteb         $COLOR_WHITEB
+    40       clr_blackb         $CLR_BLACKB
+    41       clr_redb           $CLR_REDB
+    42       clr_greenb         $CLR_GREENB
+    43       clr_brownb         $CLR_BROWNB
+    44       clr_blueb          $CLR_BLUEB
+    45       clr_magentab       $CLR_MAGENTAB
+    46       clr_cyanb          $CLR_CYANB
+    47       clr_whiteb         $CLR_WHITEB
 '
 }
 
